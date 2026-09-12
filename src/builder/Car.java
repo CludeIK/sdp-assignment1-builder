51f46966c1ae0735f8b8333119eb8d4ea9937ec1
CludeIK <kabyeviman08@gmail.com>
2026-09-12 20:05:03 +0500

    Add Product class (Car) and Enums (CarType, Engine) to eliminate magic strings

---
+++ src/builder/Car.java
@@ -0,0 +1,23 @@
+package builder;
+
+public class Car {
+    private final CarType type;
+    private final int seats;
+    private final Engine engine;
+    private final boolean hasGPS;
+    private final boolean hasTripComputer;
+
+    public Car(CarType type, int seats, Engine engine, boolean hasGPS, boolean hasTripComputer) {
+        this.type = type;
+        this.seats = seats;
+        this.engine = engine;
+        this.hasGPS = hasGPS;
+        this.hasTripComputer = hasTripComputer;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "type=" + type + "seats=" + seats + ", egine=" +
+                engine + ", hasGPS=" + hasGPS + ", hasTripComputer" + hasTripComputer + "}";
+    }
+}
